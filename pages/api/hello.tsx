f783e9d48fb2f802d4c3b2dcb11ba7806054d9f7
kekebeen <bbabic081@gmail.com>
2020-11-27 22:37:52 +0100

    refactor(hello): add request and response types

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+interface Data {
+  name: string;
 }
+
+export const Hello = (_req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.statusCode = 200;
+  res.json({ name: 'John Doe' });
+};
+
+// eslint-disable-next-line import/no-default-export
+export default Hello;
