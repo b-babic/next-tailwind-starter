c7ed13b4e07c970391a9e1a99f4a74d72c52dc13
kekebeen <bbabic081@gmail.com>
2020-11-27 22:20:30 +0100

    refactor(homepage): add types and default export
    n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { AppProps } from 'next/app';
+import React from 'react';
 
+export const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+// eslint-disable-next-line import/no-default-export
 export default MyApp;
